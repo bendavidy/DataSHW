037293a47cb2249feb040911734cc638134d3adb
Yoad Ben David <bendavidy2@gmail.com>
2025-01-02 01:45:29 +0200

    Half of split()

--- AVLTreeUpToDate.py
+++ AVLTreeUpToDate.py
@@ -734,6 +734,7 @@ class AVLTree(object):
 			node.update_height()
 			self = lower
 			self.check_heights(node)
+			# TODO: FIX THIS to balance like in insert() and not delete()
 
 		return
 
@@ -749,7 +750,45 @@ class AVLTree(object):
 	dictionary smaller than node.key, and right is an AVLTree representing the keys in the 
 	dictionary larger than node.key.
 	"""
-	def split(self, node):
+	# TODO: IMPORTANT - delete "node : AVLNode" from signature
+	def split(self, node : AVLNode):
+		h = self.get_root().get_height()
+		smallers = [AVLNode for i in range(h)] #TODO: replace AVLNode with None in both arrays instatiations
+		biggers = [AVLNode for i in range(h)]
+		search = self.get_root()
+		# Building two arrays, of the bigger and smaller nodes in the path from the root to our split node
+		while search.get_key() != node.get_key():
+			if node.get_key() < search.get_key():
+				biggers.append(search)
+				search = search.get_left()
+			else:
+				smallers.append(search)
+				search = search.get_right()
+		if smallers[-1] is not None:
+			smallers.append(node.get_left())
+		if biggers[-1] is not None:
+			biggers.append(node.get_right())
+		# Now build the two trees from these arrays, in reverse order - to maintain small time complexity
+		i = h-1
+		while smallers[i] is None:
+			i-=1
+		j = h-1
+		while biggers[j] is None:
+			j -= 1
+		# i holds the index of last item in smallers, and j is the index of the last item in biggers
+		small = AVLTree()
+		small.root = smallers[i]
+		while i > 0:
+			subTree = AVLTree()
+			subTree.root = smallers[i-1].get_left()
+			small.join(subTree,smallers[i-1].get_key(),smallers[i-1].get_value())
+			i-=1
+		# Now same for big:
+		big = AVLTree()
+		# TODO: finish this
+
+
+		
 		return None, None
 
 	
